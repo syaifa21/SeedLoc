2ccbf5d2a1d3e2c689a8cdabdfd798682b1ffe80
Ali <alisyaifarudin04@gmail.com>
2025-11-13 15:39:25 +0700

    splas

--- seedloc/lib/screens/splash_screen.dart
+++ seedloc/lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 1), () {
+    Timer(const Duration(seconds: 5), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (_) => const HomeScreen()),
       );
